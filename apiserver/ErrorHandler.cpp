9534219f5023163b14586f3e6e120e44746756c1
Rodney Woollett <rodney.woollett@gmail.com>
2025-08-17 19:04:20 +1200

    prep for submodule apiserver

--- apiserver/ErrorHandler.cpp
+++
@@ -1,8 +0,0 @@
-#include "ErrorHandler.h"
-#include <iostream>
-
-// Report a failure
-void Rest::fail(boost::beast::error_code ec, char const *what)
-{
-  std::cerr << what << ": " << ec.message() << "\n";
-}
